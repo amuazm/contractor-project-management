e6007d0f6681db6bd7adc31faee57647977965a0
amuazm <amuazm@gmail.com>
2022-08-11 13:32:15 +0800

    eac

--- Code/code2_eac.py
+++ Code/code2_eac.py
@@ -11,17 +11,17 @@ wb_result = load_workbook(dest)
 ws_reports = wb_result["Reports"]
 ws_budget = wb_result["Budget"]
 
-# # Months Passed
-# ws_reports.insert_cols(4)
-# ws_reports["D1"] = "Months Passed"
-# i = 1
-# current_project = ""
-# for row in ws_reports.iter_rows(min_row=2):
-#     if row[0].value != current_project:
-#         current_project = row[0].value
-#         i = 1
-#     row[3].value = i
-#     i += 1
+# Months Passed
+ws_reports.insert_cols(3)
+ws_reports["C1"] = "Months Passed"
+i = 1
+current_project = ""
+for row in ws_reports.iter_rows(min_row=2):
+    if row[0].value != current_project:
+        current_project = row[0].value
+        i = 1
+    row[2].value = i
+    i += 1
 
 # # EAC
 # ws_reports.insert_cols(12)
@@ -31,16 +31,17 @@ ws_budget = wb_result["Budget"]
 #     row[11].value = row[4].value + (overall_budgets[row[0].value] - row[5].value)
 #     row[11].style = "Currency"
 
+# Overall Budgets dict
 overall_budgets = {}
 for row in ws_budget.iter_rows(min_row=2):
     overall_budgets[row[0].value] = row[4].value
 
 # EAC/CPI = ACWP + (Overall Budget - BCWP) / CPI
-ws_reports.insert_cols(11)
-ws_reports["K1"].value = "EAC"
+ws_reports.insert_cols(12)
+ws_reports["L1"].value = "EAC"
 for row in ws_reports.iter_rows(min_row=2):
-    row[10].value = row[3].value + (overall_budgets[row[0].value] - row[4].value) / row[6].value
-    row[10].style = "Currency"
+    row[11].value = row[4].value + (overall_budgets[row[0].value] - row[5].value) / row[7].value
+    row[11].style = "Currency"
 
 # # EAC/SPI
 # ws_reports.insert_cols(14)
@@ -86,11 +87,17 @@ for row in ws_reports.iter_rows(min_row=2):
 # for row in ws_reports.iter_rows(min_row=2):
 #     row[18].value = row[3].value + (max(durations[row[0].value], row[3].value) - row[3].value * row[9].value)
 
-# # EAC(t)(ED)/SPI
-# ws_reports.insert_cols(20)
-# ws_reports["T1"].value = "EAC(t)(ED)/SPI"
-# for row in ws_reports.iter_rows(min_row=2):
-#     row[19].value = row[3].value + (max(durations[row[0].value], row[3].value) - row[3].value * row[9].value) / row[9].value
+# Project durations dict
+project_durations = {}
+for row in ws_budget.iter_rows(min_row=2):
+    project_durations[row[0].value] = row[2].value
+
+# EAC(t)(ED)/SPI = Months Passed + (max(Duration (Months), Months Passed) - ED) / SPI
+# ED = Months Passed * SPI
+ws_reports.insert_cols(13)
+ws_reports["M1"].value = "EAC(t)"
+for row in ws_reports.iter_rows(min_row=2):
+    row[12].value = row[2].value + (max(project_durations[row[0].value], row[2].value) - row[2].value * row[9].value) / row[9].value
 
 # # EAC(t)(ED)/SCI
 # ws_reports.insert_cols(21)
@@ -98,41 +105,32 @@ for row in ws_reports.iter_rows(min_row=2):
 # for row in ws_reports.iter_rows(min_row=2):
 #     row[20].value = row[3].value + (max(durations[row[0].value], row[3].value) - row[3].value * row[9].value) / (row[7].value * row[9].value)
 
-# # Remove Unnecessary Columns for Simplicity
-# ws_reports.delete_cols(12, 1)
-# ws_reports.delete_cols(13, 2)
-# ws_reports.delete_cols(13, 1)
-# ws_reports.delete_cols(13, 3)
-# ws_reports.delete_cols(14, 1)
-
-# # # ETC
-# # ws_reports.insert_cols(14)
-# # ws_reports["N1"].value = "ETC"
-# # # ETC = EAC - ACWP
-# # for row in ws_reports.iter_rows(min_row=2):
-# #     row[13].value = row[11].value - row[4].value
-# #     row[13].style = "Currency"
-
-# # VAC
+# # ETC
 # ws_reports.insert_cols(14)
-# ws_reports["N1"].value = "VAC"
-# # VAC = Overall Budget - EAC
+# ws_reports["N1"].value = "ETC"
+# # ETC = EAC - ACWP
 # for row in ws_reports.iter_rows(min_row=2):
-#     row[13].value = overall_budgets[row[0].value] - row[11].value
+#     row[13].value = row[11].value - row[4].value
 #     row[13].style = "Currency"
 
-# # VAC(t)
-# ws_reports.insert_cols(15)
-# ws_reports["O1"].value = "VAC(t)"
-# # VAC(t) = Duration (Months) - EAC(t)
+# VAC = Overall Budget - EAC
+ws_reports.insert_cols(14)
+ws_reports["N1"].value = "VAC"
+for row in ws_reports.iter_rows(min_row=2):
+    row[13].value = overall_budgets[row[0].value] - row[11].value
+    row[13].style = "Currency"
+
+# VAC(t) = Duration (Months) - EAC(t)
+ws_reports.insert_cols(15)
+ws_reports["O1"].value = "VAC(t)"
+for row in ws_reports.iter_rows(min_row=2):
+    row[14].value = project_durations[row[0].value] - row[12].value
+
+# # TCPI
+# ws_reports.insert_cols(16)
+# ws_reports["P1"].value = "TCPI"
+# # TCPI = (Overall Budget - BCWP)/(Overall Budget - ACWP)
 # for row in ws_reports.iter_rows(min_row=2):
-#     row[14].value = durations[row[0].value] - row[12].value
+#     row[15].value = (overall_budgets[row[0].value] - row[5].value)/(overall_budgets[row[0].value] - row[4].value)
 
-# # # TCPI
-# # ws_reports.insert_cols(16)
-# # ws_reports["P1"].value = "TCPI"
-# # # TCPI = (Overall Budget - BCWP)/(Overall Budget - ACWP)
-# # for row in ws_reports.iter_rows(min_row=2):
-# #     row[15].value = (overall_budgets[row[0].value] - row[5].value)/(overall_budgets[row[0].value] - row[4].value)
-
-# wb_result.save(dest)
+wb_result.save(dest)
